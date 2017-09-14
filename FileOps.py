91bc8bdeeb73d3aa328b8cf20f902c8df5a48ff2
Harshana Serasinghe <harshanadabezt@gmail.com>
2017-09-14 10:09:42 +0530

    added which property is missing, added coloured text to terminal

--- FileOps.py
+++ FileOps.py
@@ -1,10 +1,13 @@
-import json,zipfile,os
+import json
+import zipfile
+import os
+from colorama import *
 
 
 class File:
-
     def __init__(self):
-        ignore_list = ['site.zip','','deploy-config.json']
+        init()
+        ignore_list = ['site.zip', '', 'deploy-config.json']
 
     def fetch_old_config(self):
         # Fetches old WP Configurations from wp-config.php
@@ -26,30 +29,45 @@ class File:
         old_config = self.fetch_old_config()
         new_config = {}
         print("Please input the following details or press enter to keep old values...")
-        new_config['db_host'] = raw_input("Remote Host Name (Old Value:'"+old_config['DB_HOST']+"'): ")
-        new_config['db_name'] = raw_input("Database Name (Old Value:'"+old_config['DB_NAME']+"'): ")
-        new_config['db_user'] = raw_input("DB User Name (Old Value:'"+old_config['DB_USER']+"'): ")
-        new_config['db_password'] = raw_input("DB Password (Old Value:'"+old_config['DB_PASSWORD']+"'): ")
+        print Style.BRIGHT
+        new_config['db_host'] = raw_input("Remote Host Name (Old Value:'" + old_config['DB_HOST'] + "'): ")
+        new_config['db_name'] = raw_input("Database Name (Old Value:'" + old_config['DB_NAME'] + "'): ")
+        new_config['db_user'] = raw_input("DB User Name (Old Value:'" + old_config['DB_USER'] + "'): ")
+        new_config['db_password'] = raw_input("DB Password (Old Value:'" + old_config['DB_PASSWORD'] + "'): ")
         new_config['site_url'] = raw_input("Remote Site URL : ")
         new_config['ftp_user'] = raw_input("FTP Username : ")
         new_config['ftp_pass'] = raw_input("FTP Password : ")
+        print Style.RESET_ALL
 
-        return old_config,new_config
+        return old_config, new_config
 
     def create_config(self):
         # Creates deploy-config.json file to use in deployment stage
+        # Returns 1 if config created successfully ELSE 0
+        strings = {'db_host': 'Remote Host Name', 'db_name': "Database Name", 'db_user': "Database User Name",
+                   'db_password': "Database Password", 'site_url': "Remote Site URL", "ftp_user": "FTP User name",
+                   "ftp_pass": "FTP Password"}
+        is_completed = 0
         old_config, new_config = self.prompt_config()
         for key, value in new_config.iteritems():
             if value == "":
                 try:
                     new_config[key] = old_config[key.upper()]  # getting values from old configs
+                    is_completed = 1
                 except:
-                    print "Some details are missing to continue the process. Please re-enter them"
-                    self.prompt_config()
-        file = open("deploy-config.json", "w")
-        file.write(json.dumps(new_config))
-        file.close()
-        print "Config Created"
+                    print Fore.RED + Back.WHITE + "\n" + strings[
+                        key] + " is missing to continue the process. Please re-enter them\n" + Fore.RESET + Back.RESET
+
+                    is_completed = 0
+                    break
+        if is_completed:
+            file = open("deploy-config.json", "w")
+            file.write(json.dumps(new_config))
+            file.close()
+            print "Config Created"
+            return 1
+        else:
+            return 0
 
     def archive_site(self):
         file_path = ""
